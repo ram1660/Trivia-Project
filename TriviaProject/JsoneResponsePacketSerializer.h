391af425f73ae5b6f6cc49592385cad074c94841
Ram Tourgeman <ramtorgeman@gmail.com>
2018-04-15 17:10:33 +0300

    Changed to stucts

--- TriviaProject/JsoneResponsePacketSerializer.h
+++ TriviaProject/JsoneResponsePacketSerializer.h
@@ -3,7 +3,7 @@
 class JsonResponsePacketSerializer
 {
 public:
-	static Buffer serializeResponse(ErrorRepose s);
+	static Buffer serializeResponse(ErrorResponse s);
 	static Buffer serializeResponse(LoginResponse s);
 	static Buffer serializeResponse(SignupResponse s);
 	
