5f45c8450ce05aaa3c74a106be5ec9dbe43f09cf
Ram Tourgeman <ramtorgeman@gmail.com>
2018-05-01 11:56:08 +0300

    Implementing the LoginRequestHandler.

--- TriviaProject/LoginRequestHandler.cpp
+++ TriviaProject/LoginRequestHandler.cpp
@@ -1,31 +1,44 @@
 #include "LoginRequestHandler.h"
-#include "structures.h"
-bool LoginRequestHandler::isRequestRelevan(Request r)
-{
-	
-	return false;
-}
-
 RequestResult LoginRequestHandler::handleRequest(Request r)
 {
-	JsonRequestPacketDeserializer::deserializeLoginRequest(r.buffer);
+	Buffer b;
+	b.buffer = r.buffer;
+	LoginRequest request =  JsonRequestPacketDeserializer::deserializeLoginRequest(b);
+	if (r.id == REQUEST_SIGNIN)
+		return login(r);
+	else if (r.id == REQUEST_SIGNUP)
+		signup(r);
 }
 
 RequestResult LoginRequestHandler::login(Request r)
 {
+	// Calls to the DB.
+	// Informing RequestResult struct and return it
+	RequestResult result;
+	result.
 	return RequestResult();
 }
 
 RequestResult LoginRequestHandler::signup(Request r)
 {
+	// Calls to the DB.
+	// Informing RequestResult struct and return it
 	return RequestResult();
+}
+
+LoginRequestHandler::LoginRequestHandler(LoginManager & manager, RequestHandlerFactory factory) : m_loginManager(&manager), m_handlerFactory(&factory)
+{
 }
 
 LoginRequestHandler::~LoginRequestHandler()
 {
+	delete m_loginManager;
+	delete m_handlerFactory;
 }
 
 bool LoginRequestHandler::isRequestRelavent(Request r)
 {
+	if (r.id == REQUEST_SIGNUP || r.id == REQUEST_SIGNIN)
+		return true;
 	return false;
 }
--- TriviaProject/LoginRequestHandler.h
+++ TriviaProject/LoginRequestHandler.h
@@ -3,9 +3,12 @@
 #include "structures.h"
 #include "RequestHandlerFactory.h"
 #include "JsonRequestPacketDeserializer.h"
+#include "Protocol.h"
+#include "structures.h"
 class LoginRequestHandler : public IRequestHandler
 {
 public:
+	LoginRequestHandler(LoginManager& manager, RequestHandlerFactory factory);
 	~LoginRequestHandler();
 	bool isRequestRelavent(Request r) override;
 	RequestResult handleRequest(Request r) override;
